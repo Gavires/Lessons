2e2d9688c003d6be0787943012fc56e06d997a19
Игорь Сариев <igor.sariev@yandex.ru>
2023-01-29 22:23:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Lessons.Topic1.Lessons;
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lesso1 = new Lesson1();
+            lesso1.WriteLine();
+        }
+    }
+}
---
+++ Topic1/Lessons/Lesson1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Topic1.Lessons
+{
+    public class Lesson1
+    {
+        public string Text { get; private set; } = "visual studio 2022";
+
+        public void WriteLine ()
+        {
+            Console.WriteLine(Text);
+            Console.ReadKey();
+        }
+    }
+}
