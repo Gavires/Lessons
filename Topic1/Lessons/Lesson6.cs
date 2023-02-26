845d3f797e3e2b45325aede2ce15b2bb2ff204ee
Игорь Сариев <igor.sariev@yandex.ru>
2023-02-26 23:20:29 +0300

    Task-6-Добавил 3 подзадачу для 6 задания

--- Topic1/Lessons/Lesson6.cs
+++ Topic1/Lessons/Lesson6.cs
@@ -1,6 +1,7 @@
 ﻿using Lessons.LessonsTopic1Core.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,9 @@ namespace Lessons.Topic1.Lessons
             TitleTask = "Решить практические задания из видео лекции использовав свое решение.\n";
             TextTask = "Задача 6.1: написать программу, которая будет принимать числа до тех пор, пока не будет введен  “0”,\n " +
                 "после получения будет выводиться сумма полученных чисел." +
-                "Задача 6.2: написать проограмму которая будет выводит сумму вклада под проценты от суммы которой ввел пользователь";
+                "Задача 6.2: написать программу, которая будет выводит сумму вклада под проценты от суммы которой ввел пользователь" +
+                "Задача 6.3: написать программу, которая будет выводить наим. число d, которое делится нацело на введенные 2 числа a и b" +
+                "Задача 6.4: написать программу, которая будет выводить произведение всех чисел из отрезка [a,b] * [c,d], где a<b, c<d";
         }
 
         public void OutputSolutionOfTask6()
@@ -46,6 +49,24 @@ namespace Lessons.Topic1.Lessons
             var percent = CheckInputValidDoubleNumber("Введите проценты по вкладу");
             var days = CheckInputPositiveIntNumber("Введите колличество дней");
             return sum * percent * days / 365;
+        }
+
+        private void MultiplicationSegments()
+        {
+            var numberOne = CheckInputPositiveIntNumber("Введите первое целое число:");
+            var numberTwo = CheckInputPositiveIntNumber("Введите втрое целое число ");
+            var flagNumber = numberTwo >= numberOne;
+            double resOne =  flagNumber ? numberTwo / numberOne : numberOne / numberTwo;
+            var result = 0;
+            if (resOne % 10 == 0)
+            {
+                result = flagNumber ? numberTwo : numberOne;
+            }
+            else
+            {
+
+            }
+            
         }
 
     }
