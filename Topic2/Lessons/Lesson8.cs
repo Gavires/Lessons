19fe0b604cd34f86de9e50a20581313ffd57c616
Игорь Сариев <igor.sariev@yandex.ru>
2023-03-25 02:31:05 +0300

    API-2 Добавил в тему 2 урок 2(8)

---
+++ Topic2/Lessons/Lesson8.cs
@@ -0,0 +1,46 @@
+﻿using Lessons.LessonsTopic1Core.Models;
+
+namespace Lessons.Topic2.Lessons
+{
+    public class Lesson8 : BaseModel
+    {
+        private int[] IntMass { get; set; } = { 1, 3, 4, 2, 5, 6, 0, 8 };
+
+        private int LengthMass { get; set; }
+
+        public Lesson8() : base()
+        {
+            TopicId = 8;
+            TitleLesson = "Создание двумерного массива и работа с ним. Практика.\n";
+            TitleTask = "Решить практические задания из видео лекции использовав свое решение.\n";
+            TextTask = "Задача 8.1: Вывести элементы массива на экран.\n" +
+                       "Задача 8.2: Вывести элементы массива на экран в обратном порядке.\n";
+            LengthMass = IntMass.Length;
+        }
+
+        public void OutputSolutionOfTask8()
+        {
+            WritelineTask();
+            Console.WriteLine("Задание 8.1: результат:");
+            WriteLineMassNormal(IntMass);
+            Console.WriteLine("Задание 8.2: результат:");
+            WriteLineMassNegative(IntMass);
+        }
+
+        private void WriteLineMassNormal(int[] intMass)
+        {
+            for (var i = 0; i < LengthMass; i++)
+            {
+                Console.WriteLine($"\t\t\t{intMass[i]}");
+            }
+        }
+
+        private void WriteLineMassNegative(int[] intMass)
+        {
+            for (var i = LengthMass - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"\t\t\t{intMass[i]}");
+            }
+        }
+    }
+}
